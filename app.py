94c9e64abd51a249668b2ecf3b9a4eab44f8ecb0
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2025-03-31 14:33:09 +0530

    experimenting with episode matching

--- app.py
+++ app.py
@@ -3,19 +3,8 @@ import logging
 import util
 
 
-def menu_system():
-    print("Enter censorship strength (1 or 2):")
-    print("1. Moderate - Only Explicit on screen exposed nudity is removed.  ")
-    print(
-        "2. Strict - Almost all on screen nudity is removed. If any story is present in said scenes, an AI generated summary is provided on a black screen later. Profane dialogues are muted and their subttiles replaced by AI generated sentences with similar meaning. "
-    )
-
-    choice = input("Enter your choice: ").strip()
-
-
 if __name__ == "__main__":
     config = Config()
-
     logging.basicConfig(
         format=config.LOGGING_FORMAT,
         datefmt=config.LOGGING_DATE_FORMAT,
@@ -30,7 +19,9 @@ if __name__ == "__main__":
     vids = util.find_videos(config.media_folder_path)
     subs = util.find_subtitles(config.media_folder_path)
     config.video_and_srt_files = util.match_video_and_subtitles(vids, subs)
-
     logging.info(f"Found {len(config.video_and_srt_files)} videos")
-
-    menu_system()
+    
+    util.get_censorship_strength()
+    logging.info(f"Selected censorship strength: {config.censorship_strength}")
+    
+    
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import pathlib
+from settings import Config
+import logging
 
 def find_videos(path):
     '''
@@ -35,3 +37,17 @@ def print_welcome_message():
 
     """
     )
+
+def get_censorship_strength():
+    config = Config()
+    print("Enter censorship stren   gth (1 or 2):")
+    print("1. Moderate - Only Explicit on screen exposed nudity is removed.  ")
+    print(
+        "2. Strict - Almost all on screen nudity is removed. If any story is present in said scenes, an AI generated summary is provided on a black screen later. Profane dialogues are muted and their subttiles replaced by AI generated sentences with similar meaning. "
+    )
+
+    choice = input("Enter your choice: ").strip()
+    if choice not in ['1', '2']:
+        print("Invalid choice. Please enter 1 or 2.")
+        return get_censorship_strength()
+    config.censorship_strength = 'MODERATE' if choice == '1' else 'STRICT'
